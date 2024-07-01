aadef2e9831255070b8e2ee56727248054f026f6
Asher <kungsasher002@gmail.com>
2024-06-30 23:37:02 -0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Emmanuel's Portfolio",
+  title: "KnackFusion",
   description: "Emmanuel's",
 };
 
